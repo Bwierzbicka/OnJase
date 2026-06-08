41d121e1eea8b07712b2e11531a8a6073150cc40
a18n15 <atognide@pm.me>
2026-06-08 17:27:11 -0400

    created models

--- app/models/user_conversation.rb
+++ app/models/user_conversation.rb
@@ -3,5 +3,5 @@ class UserConversation < ApplicationRecord
   belongs_to :user2, class_name: "User", foreign_key: :user_id_2
   validates :user_id_1, uniqueness: { scope: :user_id_2 }
   validates :user_id_2, presence: true
-  validates :content, presence: true
+  has_many :user_conversation_messages, dependent: :destroy
 end
