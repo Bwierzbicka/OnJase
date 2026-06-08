517f69f9bc7661ab370e835ef4ba7a0dd8d24c6b
Beata Wierzbicka <b.wierzbicka90@gmail.com>
2026-06-08 17:31:37 -0400

    updated validations and removed translation column

--- app/models/saved_item.rb
+++ app/models/saved_item.rb
@@ -1,3 +1,4 @@
 class SavedItem < ApplicationRecord
   belongs_to :saveable, polymorphic: true
+  belong_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_08_205433) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_08_212335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,7 +18,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_08_205433) do
     t.datetime "created_at", null: false
     t.string "english"
     t.string "french"
-    t.string "translation"
     t.datetime "updated_at", null: false
   end
 
