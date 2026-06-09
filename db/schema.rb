2acdd71fcb21dbfb32d96b047308548919c5a625
djibril sadoun <cyberpsycho012@gmail.com>
2026-06-09 17:21:13 -0400

    26-make-additions-to-user-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[8.1].define(version: 2026_06_08_212800) do
->>>>>>> user1
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
