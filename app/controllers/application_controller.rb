fb50f4f38a02e2a7932cc4e7a05dd423195c6063
Thomas <thomasdanielrice@proton.me>
2026-06-15 16:25:37 -0400

    disable devise for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  PUNDIT_SKIP_CONTROLLERS = %w[pages dashboard chats].freeze
   include Pundit::Authorization
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def after_sign_in_path_for(resource)
     dashboard_path
